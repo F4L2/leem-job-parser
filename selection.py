c4cc657a42f4194c0511b376dd13c184edde2682
Alex <alex.ye@outlook.fr>
2020-11-04 18:26:19 +0100

    table md format

--- selection.py
+++ selection.py
@@ -8,6 +8,25 @@ import datamining
 
 
 
+def pandas_df_to_markdown_table(df):
+    # Dependent upon ipython
+    # shamelessly stolen from https://stackoverflow.com/questions/33181846/programmatically-convert-pandas-dataframe-to-markdown-table
+    from IPython.display import Markdown, display
+    fmt = ['---' for i in range(len(df.columns))]
+    df_fmt = pd.DataFrame([fmt], columns=df.columns)
+    df_formatted = pd.concat([df_fmt, df])
+    #display(Markdown(df_formatted.to_csv(sep="|", index=False)))
+    return Markdown(df_formatted.to_csv(sep="|", index=False))
+#     return df_formatted
+
+def df_to_markdown(df, y_index=False):
+    blob = tabulate(df, headers='keys', tablefmt='pipe')
+    if not y_index:
+        # Remove the index with some creative splicing and iteration
+        return '\n'.join(['| {}'.format(row.split('|', 2)[-1]) for row in blob.split('\n')])
+    return blob
+
+
 
 selection = []
 
@@ -79,6 +98,8 @@ with open('readme.md', 'w') as f:
     print("A Web parser to browse more easily the job post for young graduate on leem.org: {}    I was consulting the fresh graduate job posting section of leem.org, while I've found some interesting jobposts, it was really annoying to browse.  So I made this little program to mine and filter junior jobposts. The results are periodically printed in a table below via a CRON that I've set to run daily.  ".format(LEEM_JEUNE_URL))
     
     
-    print(df.to_markdown(tablefmt="grid"))
+    # print(df.to_markdown(tablefmt="grid"))
+    print( pandas_df_to_markdown_table(df) )
+
     sys.stdout = original_stdout # reset stdout
     
